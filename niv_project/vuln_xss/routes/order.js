b54e59db080839fa8d1b26747c2f84675bfb8c09
marin-jovanovic <marin.jovanovic12@gmail.com>
2023-01-28 21:09:10 +0100

    xss reflected order

---
+++ niv_project/vuln_xss/routes/order.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+const {
+    getFileContent
+} = require("../js/file_reader");
+
+const {
+    injectIntoTemplate
+} = require("../js/template_loader");
+
+
+
+router.get('/', function(req, res) {
+
+    // router.get('/', function(req, res) {
+
+    let content = "fff";
+
+    let navbar = injectIntoTemplate(
+        getFileContent("./view_components/navbar.html"),
+        [
+            getFileContent("./view_components/navbar_private_links.html"),
+            getFileContent('./view/order.html')
+        ]
+    )
+
+    let r = injectIntoTemplate(
+        getFileContent("./view_components/base.html"),
+        [navbar]
+    )
+
+    res.send(r);
+    
+    
+        // res.send(`
+        //     <form action="/paymentCSRF" method="post">
+        //     <input name="csrfToken" value="${csrfToken}" hidden>
+        //     <input name="bankNumber" placeholder="bank number">
+        //     <input name="amount" placeholder="amount">
+        //     <button>pay</button>
+        //     </form>
+    
+        // `);
+    
+    // });
+});
+
+
+router.get('/:id', function(req, res) {
+
+//   reflected
+
+    let orderStatus = "Dispatched"
+
+// escaped is safe
+
+    res.send(
+        `
+        <h1> Order status </h1>
+        <h2> Order id: ${escape(req.params.id)} <h2>
+        <h2> Order id: ${req.params.id} <h2>
+        <h2> Order status: ${escape(orderStatus)} <h2>        
+       `
+    );
+
+});
+
+module.exports = router;
--- niv_project/vuln_xss/server.js
+++ niv_project/vuln_xss/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const escape = require('escape-html');
+// const path = require('path');
+// const escape = require('escape-html');
 
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
@@ -10,8 +10,8 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-var cors = require('cors')
-app.use(cors())
+// var cors = require('cors')
+// app.use(cors())
 
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
@@ -19,37 +19,7 @@ app.use('/payment', require('./routes/payment'));
 app.use('/paymentSecure', require('./routes/payment_secure'));
 app.use('/viewPost', require('./routes/post_view'));
 app.use('/createPost', require('./routes/post_create'));
-
-
-app.get('/track_order/:id', function(req, res) {
-
-    // reflected
-
-    res.send(
-        `
-
-        <script>
-    
-        sessionStorage.setItem("username", "john");
-        sessionStorage.setItem("sessionToken", "123");
-
-        console.log(
-            sessionStorage.getItem("username") + "/" + 
-            sessionStorage.getItem("sessionToken")
-        );
-
-        </script>
-
-        safe: <h1> hello ${escape(req.params.id)} </h1>
-
-        unsafe: <h1> hello ${req.params.id} </h1>
-
-        `
-
-    )
-
-});
-
+app.use('/trackOrder', require('./routes/order'));
 
 app.listen(3000);
 console.log("listening on http://localhost:3000");
